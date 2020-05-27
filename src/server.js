8ad1f5a809a3feddec5bf940f84efee1e90b98fb
coishi59 <coishi59@msn.com>
2020-05-26 19:40:08 -0500

    se cambia a puerto 4000

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const session = require('express-session');
 const app = express();
 
 //settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
 
 app.engine('.hbs', exphbs({
